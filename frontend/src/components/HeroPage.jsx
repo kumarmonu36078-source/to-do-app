31c9178f16497caa4935b713dcf030624a3dd80e
Monu Kumar <kumarmonu36078@gmail.com>
2026-06-16 10:15:40 +0530

    img change

--- frontend/src/components/HeroPage.jsx
+++ frontend/src/components/HeroPage.jsx
@@ -5,7 +5,7 @@ import webp3 from '../assets/webp3.webp';
 
 const HeroPage = () => {
   return (
-    <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+    <div id="carouselExampleCaptions" className="carousel slide d-none d-sm-block" data-bs-ride="carousel">
       {/* Indicators (Niche ke dots/bars) */}
       <div className="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
